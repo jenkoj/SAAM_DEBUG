1f4ae188bf2e557881f53dd81101e025aa7b14ee
Jakob <jakob.jenko@gmail.com>
2020-08-18 13:04:14 +0200

    added aut

---
+++ auto.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash -c 'cd || cd SAAM_DEBUG || cp saam-logger.service /lib/systemd/system/ || chmod +x rssi_logger.py || systemctl enable saam-logger.service || systemctl start saam-logger.service'
